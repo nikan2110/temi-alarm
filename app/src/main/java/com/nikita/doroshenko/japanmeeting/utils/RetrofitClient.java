2f74add431ee3677e80c610ff1ba6bef10154b26
Nikita Doroshenko <nikan2110@gmail.com>
2023-03-28 11:02:09 +0300

    add scroll view to check list

--- app/src/main/java/com/nikita/doroshenko/japanmeeting/utils/RetrofitClient.java
+++ app/src/main/java/com/nikita/doroshenko/japanmeeting/utils/RetrofitClient.java
@@ -20,7 +20,7 @@ public class RetrofitClient {
 
         if (retrofit==null) {
             retrofit = new Retrofit.Builder()
-                    .baseUrl("http://172.18.0.181:8000/temi/")
+                    .baseUrl("http://172.18.0.225:8000/temi/")
                     .client(okHttpClient)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
